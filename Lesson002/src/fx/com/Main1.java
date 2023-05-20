91d907f6cf500b130db0ee62e48d3c07fab08ec7
SHI YU <459123565@qq.com>
2023-05-21 06:04:04 +0800

    Lesson002

---
+++ Lesson002/src/fx/com/Main1.java
@@ -0,0 +1,16 @@
+package fx.com;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("这是start方法");
+    }
+}
---
+++ Lesson002/src/fx/com/Main2.java
@@ -0,0 +1,17 @@
+package fx.com;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main2 extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(Main2.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("这是start方法");
+    }
+
+}
